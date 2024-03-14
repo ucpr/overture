84dce79d035249be7618a40697180106cce22f68
ucpr <contact@ucpr.dev>
2024-03-15 01:46:02 +0900

    feat: add clap cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // init command is used to initialize a new project
+    #[arg(short, long)]
+    init: String,
+
+    // build command is used to build the project
+    #[arg(short, long)]
+    build: String,
+
+    // serve command is used to serve the project on local server
+    #[arg(short, long)]
+    serve: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.serve != "" {
+        println!("Serving on port {}", args.serve);
+    }
+    if args.build != "" {
+        println!("Building for {}", args.build);
+    }
+    if args.init != "" {
+        println!("Initializing a new project in {}", args.init);
+    }
 }
