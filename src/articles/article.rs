5fd151f28ccf967b239447bd10aed13a11e74a56
ucpr <contact@ucpr.dev>
2024-04-26 17:42:21 +0900

    feat: impl ExternalArticle

--- src/articles/article.rs
+++ src/articles/article.rs
@@ -1,17 +1,22 @@
 use serde::Serialize;
 
+use crate::articles::external;
+use crate::articles::local;
+
 #[derive(Debug, Clone, Serialize)]
 pub enum Source {
-    Unknown,
-    Zenn,
-    ZennScraps,
-    HatenaBlog,
+    Unknown,    // 不明
+    Local,      // overture で生成された記事
+    Zenn,       // Zenn の記事
+    ZennScraps, // Zenn のスクラップ
+    HatenaBlog, // はてなブログ
 }
 
 impl ToString for Source {
     fn to_string(&self) -> String {
         match self {
             Source::Unknown => "Unknown".to_string(),
+            Source::Local => "Local".to_string(),
             Source::Zenn => "Zenn".to_string(),
             Source::ZennScraps => "Zenn Scraps".to_string(),
             Source::HatenaBlog => "HatenaBlog".to_string(),
@@ -19,5 +24,9 @@ impl ToString for Source {
     }
 }
 
-pub struct Article {
+pub struct Articles {
+    local_articles: Vec<local::LocalArticle>,
+    external_articles: external::ExternalArticles,
 }
+
+impl Articles {}
